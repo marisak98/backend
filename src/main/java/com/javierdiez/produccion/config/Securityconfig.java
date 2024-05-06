a61f0921a08dccd40ee8042e8f47cc94de7cb25f
marisak98 <sanchezjordan.sh@gmail.com>
2024-05-06 16:05:17 -0500

    config security applied.

--- src/main/java/com/javierdiez/produccion/config/Securityconfig.java
+++ src/main/java/com/javierdiez/produccion/config/Securityconfig.java
@@ -3,6 +3,30 @@ package com.javierdiez.produccion.config;
 /**
  * Securityconfig
  */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
 public class Securityconfig {
+
+  @Bean
+  DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(auth -> {
+          auth
+              .requestMatchers("/").permitAll()
+              .anyRequest().authenticated();
+        })
+        .oauth2Login(withDefaults())
+        .formLogin(withDefaults())
+        .build();
+  }
 
 }
